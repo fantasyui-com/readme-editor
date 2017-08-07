9c1e65e4e4ebaf361c2e98184bbc1082c9429ddd
Captain Fantasy <captain@fantasyui.com>
2017-08-07 12:27:51 -0400

    Full UI

--- renderer.js
+++ renderer.js
@@ -1,3 +1,23 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const _ = require('lodash');
+const marked = require('marked');
+
+new Vue({
+  el: '#editor',
+  data: {
+    input: '# Readme Editor'
+  },
+  computed: {
+    compiledMarkdown: function () {
+      return marked(this.input, { sanitize: true })
+    }
+  },
+  methods: {
+    update: _.debounce(function (e) {
+      this.input = e.target.value
+    }, 300)
+  }
+})
