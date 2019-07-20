a974f7347350838a291cd1c32cf3f57895bd72e9
fantasyui-com <captain@fantasyui.com>
2019-07-20 18:28:49 -0400

    Drop Preload in favor of Node Integration

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+                nodeIntegration: true
+            }
   })
 
   // and load the index.html of the app.
